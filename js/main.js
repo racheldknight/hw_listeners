7a7508ffabab4a583784d4c50bea3b433471105a
Rachel Knight <dinkins.rachel@gmail.com>
2015-02-28 12:25:40 -0500

    Added countnumber to click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var heading = document.getElementsByTagName("h1");
+var count = 0;
 
 heading[0].addEventListener( "click", function () {
 	var newPara = document.createElement( "p" );
-	newPara.innerHTML = "This is click number x";
+	count += 1;
+	newPara.innerHTML = "This is click number " +count;
 
 	document.body.appendChild( newPara );
 });
