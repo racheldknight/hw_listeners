c07f135e305d16e22bacb5046b06a6828b8cb495
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-04 18:59:32 -0500

    log out correct message

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var myForm = document.getElementById("form");
 
 myForm.addEventListener( "submit", function (event) {
-	event.preventDefault();
-	var firstname = document.getElementById( "firstname" ).value;
-	var lastname = document.getElementById( "lastname" ).value;
-	var email = document.getElementById( "email" ).value;
-	var message = document.getElementById( "message" ).value;
-	console.log (firstname.value);
-	console.log (lastname.value);
-	console.log (email.value);
-	console.log (message.value);
+    event.preventDefault();
+    var firstname = document.getElementById( "firstname" ).value;
+    var lastname = document.getElementById( "lastname" ).value;
+    var email = document.getElementById( "email" ).value;
+    var message = document.getElementById( "message" ).value;
+    console.log(firstname);
+    console.log(lastname);
+    console.log(email);
+    console.log(message);
 });
 
